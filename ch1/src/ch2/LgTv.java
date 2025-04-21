8afdc96d48e00995464ba487a0fa81faa59dfaa4
joung dong jin <jdj5201@gmail.com>
2025-04-21 16:01:28 +0900

    객체 그래프 탐색 예제

---
+++ ch1/src/ch2/LgTv.java
@@ -0,0 +1,44 @@
+package ch2;
+
+// LgTv에 선언된 멤버 변수 초기화
+// 1) 생성자
+// 2) setter
+
+public class LgTv implements Tv {
+
+    private Speaker speaker; // null
+
+    public LgTv(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    public LgTv() {
+    }
+
+    // setter 메소드
+    public void setSpeaker(Speaker speaker) {
+        this.speaker = speaker;
+    }
+
+    @Override
+    public void powerOn() {
+        System.out.println("LgTv - 전원 On");
+    }
+
+    @Override
+    public void powerOff() {
+        System.out.println("LgTv - 전원 Off");
+    }
+
+    @Override
+    public void volumeUp() {
+        // System.out.println("LgTv - 볼륨 Up");
+        speaker.volumeUp();
+    }
+
+    @Override
+    public void volumeDown() {
+        // System.out.println("LgTv - 볼륨 Down");
+        speaker.volumeDown();
+    }
+}
